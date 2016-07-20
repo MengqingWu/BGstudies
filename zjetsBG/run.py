1c68e058760872fed48f6beaacb8d2ffce4f7e8c
MengqingWu <mewu@cern.ch>
2016-07-20 14:53:25 +0200

    one bin 0-0.1 lost in shapeCorrectionsCRs2d.py, bug fixed

--- zjetsBG/run.py
+++ zjetsBG/run.py
@@ -3,7 +3,7 @@ from stack_zjets import *
 
 t=StackZjetsDD( indir_dd="./METSkim_v5", zpt_cut='100', met_cut= '50',scaleDphi=True)
 #rab, rac, rad= t.getYieldCorr()
-t.getAllmcRegA()
+#t.getAllmcRegA()
 #t.drawDataDrivenStack()
 # t.ValidateDphiShapeCorr("./METSkim_v4", whichvar='fabsDphi',isNormalized=False, yieldCorr=True, whichbcd='ZJets',scaleDphi=True,onlyStats=False)
 # t.ValidateDphiShapeCorr("./METSkim_v4", whichvar='met',     isNormalized=False, yieldCorr=True, whichbcd='ZJets',scaleDphi=True,onlyStats=False)
