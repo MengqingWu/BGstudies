29e24a790a34a4cda3ffde5943901f131db20113
MengqingWu <mewu@cern.ch>
2016-08-10 18:45:05 +0200

    update

--- python/SetCuts.py
+++ python/SetCuts.py
@@ -13,20 +13,24 @@ class SetCuts ():
         self.met_pt_in=raw_input("[info] 'SetCuts' -> please give the MET cut: (enter to use default MET>100GeV)\n")
         self.met_pt='100' if self.met_pt_in=='' else self.met_pt_in
 
-        self.var1='fabs(fabs(llnunu_deltaPhi)-TMath::Pi()/2)'
-        self.var2='cos(llnunu_deltaPhi)/fabs(cos(llnunu_deltaPhi))'
-
-        self.cut_var1 ='TMath::Pi()/4'
-        self.cut_var2 ='0' 
+        #self.var1='fabs(fabs(llnunu_deltaPhi)-TMath::Pi()/2)'
+        #self.var2='cos(llnunu_deltaPhi)/fabs(cos(llnunu_deltaPhi))'
+        self.zj_var = 'fabs(llnunu_deltaPhi)'
+        
+        #self.cut_var1 ='TMath::Pi()/4'
+        #self.cut_var2 ='0' 
+        self.cut_zj_var = ['0', 'TMath::Pi()/4', 'TMath::Pi()/2','3*TMath::Pi()/4']
         
         # define a cutflow for signal region
         self.cutflow=("(nllnunu>0)", #0
                       "(llnunu_l1_mass>70.0&&llnunu_l1_mass<110.0)", #1
                       "llnunu_l1_pt>100.0", #2
                       "llnunu_l2_pt>"+self.met_pt, #3
-                      self.var1+'>'+self.cut_var1, #4
-                      self.var2+'<'+self.cut_var2, #5
-                      "nlep<3")  #6
+                      "nlep<3", #4
+                      self.zj_var+'>'+self.cut_zj_var[3], #5
+                      #self.var1+'>'+self.cut_var1, #4
+                      #self.var2+'<'+self.cut_var2, #5
+                      ) 
 
     def GetSRCut(self, N_minus_1=''):
         """ N_minus_1 (from 1 to 5) is to choose which cut to loose to get the N-1 plots  """
@@ -49,8 +53,7 @@ class SetCuts ():
         zpt+=zpt_cut if zpt_cut!='' else '100.0'
         met+=met_cut if met_cut!='' else self.met_pt
                  
-        astr = "({0}&&{4}&&{5}&&{6})"
-        #astr = "({0}&&{4}&&{5})"
+        astr = "({0}&&{4}&&{5})"
         cuts_tmp = astr.format(*self.cutflow)
         cuts_tmp+='&&'+zpt+'&&'+met
 
@@ -85,39 +88,45 @@ class SetCuts ():
         if channel in ['el', 'mu', 'inclusive'] : Channel=channel
         else:
             Channel='inclusive'
-            print "[Warning] abcdCuts(): ",channel ," is not in ['el', 'mu', 'inclusive'], choose default 'inclusive'."
+            print "[Warning] abcdCuts(): invalid value for 'channel', choose default = 'inclusive'."
 
         channelCut={'el':'abs(llnunu_l1_l1_pdgId)==11',
                     'mu':'abs(llnunu_l1_l1_pdgId)==13',
                     'inclusive': '(1)'}
+
+        zpt='llnunu_l1_pt>'
         
-        zpt=zpt_cut if zpt_cut!='' else '100.0'
-        met=met_cut if met_cut!='' else self.met_pt
+        if whichRegion=="":
+            whichRegion=raw_input("[info]' abcdCuts' -> Please choose a benchmarck Region (SR or VR): \n")
+            if whichRegion not in ['SR','VR']: whichRegion='SR'; print "[Warning] invalid 'whichRegion' input, choose default = 'SR'."
+            
+        if whichRegion=='SR': met='llnunu_l2_pt>'
+        elif whichRegion=='VR': met='llnunu_l2_pt>50.0&&llnunu_l2_pt<'
+        else:
+            print "I do not understand your benchmark Region, should be either 'SR' for MET>"+met+"GeV or 'VR' for 50.0<MET<"+met+"GeV\n"
+            sys.exit(0)
+
+        zpt=zpt+zpt_cut if zpt_cut!='' else zpt+'100.0'
+        met=met+met_cut if met_cut!='' else met+self.met_pt
 
         #fakeMetCut='llnunu_l1_pt/llnunu_mta<0.7&&dPhi_jetMet_min_a>0.4'
         #preSelection='nllnunu>0&&(llnunu_l1_mass>70.0&&llnunu_l1_mass<110.0)&&llnunu_l1_pt>'
-        prestr = "({0}&&{1}&&{6})"
-        preSelection = prestr.format(*self.cutflow)
-        if extra_cut: preSelection+='&&('+extra_cut+')'
-                        
-        if whichRegion=="": whichRegion=raw_input("[info]' abcdCuts' -> Please choose a benchmarck Region (SR or VR): \n")
-        if whichRegion=='SR':
-            preSelection='('+preSelection+'&&llnunu_l1_pt>'+zpt+'&&llnunu_l2_pt>'+met+')'
-        elif whichRegion=='VR':
-            preSelection='('+preSelection+'&&llnunu_l1_pt>'+zpt+'&&llnunu_l2_pt<'+met+')'
-        else:
-            print "I do not understand your benchmark Region, should be either 'SR' for MET>"+met+"GeV or 'VR' for MET<"+met+"GeV\n"
-            sys.exit(0)
-            
+        
+        preSelection = "{0}&&{1}&&{4}".format(*self.cutflow)
+        regPreSelect=[preSelection, zpt, met, channelCut[Channel]]
+        if extra_cut: regPreSelect.append(extra_cut)
 
         if isPreSelect:
-            cuts='('+preSelection+'&&'+channelCut[Channel]+')'
+            cuts='('+'&&'.join(regPreSelect)+')'
         else:
-            cuts_a='('+preSelection+'&&'+channelCut[Channel]+'&&'+self.var1+'>'+self.cut_var1+'&&'+self.var2+'<'+self.cut_var2+')'
-            cuts_b='('+preSelection+'&&'+channelCut[Channel]+'&&'+self.var1+'>'+self.cut_var1+'&&'+self.var2+'>'+self.cut_var2+')'
-            cuts_c='('+preSelection+'&&'+channelCut[Channel]+'&&'+self.var1+'<'+self.cut_var1+'&&'+self.var2+'<'+self.cut_var2+')'
-            cuts_d='('+preSelection+'&&'+channelCut[Channel]+'&&'+self.var1+'<'+self.cut_var1+'&&'+self.var2+'>'+self.cut_var2+')'
-            cuts=OrderedDict({'regA':cuts_a, 'regB':cuts_b, 'regC':cuts_c, 'regD':cuts_d})
+            cuts_a=regPreSelect + [self.zj_var + '>' +self.cut_zj_var[3]]
+            cuts_b=regPreSelect + [self.zj_var + '>' +self.cut_zj_var[2], self.zj_var + '<=' + self.cut_zj_var[3]]
+            cuts_c=regPreSelect + [self.zj_var + '>' +self.cut_zj_var[1], self.zj_var + '<=' + self.cut_zj_var[2]]
+            cuts_d=regPreSelect + [self.zj_var + '>' +self.cut_zj_var[0], self.zj_var + '<=' + self.cut_zj_var[1]]
+            cuts=OrderedDict({'regA': '(' + '&&'.join(cuts_a) + ')',
+                              'regB': '(' + '&&'.join(cuts_b) + ')',
+                              'regC': '(' + '&&'.join(cuts_c) + ')',
+                              'regD': '(' + '&&'.join(cuts_d) + ')'})
         
         return cuts
    
